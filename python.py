73b3175a05534205ab91ba0d5474a12444bdfb37
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2022-09-13 15:40:00 +0200

    done

---
+++ python.py
@@ -0,0 +1,12 @@
+
+n = int(input("napis cislo: "))
+print(n)
+
+for num in range(n + 1):
+  
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
